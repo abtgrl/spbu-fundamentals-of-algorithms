7d1ce14227a23120a65cf6d81f16b661594ec554
darya <da6ayar@gmail.com>
2023-04-14 23:46:39 +0300

    solved practicum 5

--- practicum_5/homework/convex_bucket.py
+++ practicum_5/homework/convex_bucket.py
@@ -9,12 +9,22 @@ from src.plotting import plot_points
 def convex_bucket(points: NDArray) -> NDArray:
     """Complexity: O(n log n)"""
     clockwise_sorted_ch = []
+    points = sorted(points, key=lambda p: (p[0], p[1]))
 
-    ##########################
-    ### PUT YOUR CODE HERE ###
-    ##########################
+    clockwise_sorted_ch.append(points[0])
+    clockwise_sorted_ch.append(points[1])
+
+    for point in points[2:]:
+        while len(clockwise_sorted_ch) > 1 and not is_ccw(clockwise_sorted_ch[-2], clockwise_sorted_ch[-1], point):
+            del clockwise_sorted_ch[-1]
+        clockwise_sorted_ch.append(point)
 
     return np.array(clockwise_sorted_ch)
+
+
+def is_clockwise(p1: NDArray, p2: NDArray, p3: NDArray) -> bool:
+    area = (p2[0] - p1[0]) * (p3[1] - p1[1]) - (p2[1] - p1[1]) * (p3[0] - p1[0])
+    return area > 0
 
 
 if __name__ == "__main__":
